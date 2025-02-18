37de373a4c6fccc85e5ecfc65e82da162185b23d
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-02-18 21:18:59 +0530

    comparision

---
+++ comparision.js
@@ -0,0 +1,24 @@
+// console.log(5>2);
+// console.log("2">1);
+// console.log("02">1)
+
+//  comparision operator  converts the string into number
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0)
+
+// comperision converts null as number and treating it as 0
+
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+//  gives false in all cases
+
+// strictly check  ===
+
+
+console.log("2"==2);  // output: false
+console.log("2"===2); // output: true
