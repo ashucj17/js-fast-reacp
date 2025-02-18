003619724a6c3c3d8e8399b136472527ad0db3ea
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-02-18 21:40:53 +0530

    premitive and non premitive datatype

---
+++ datatype.js
@@ -0,0 +1,35 @@
+// primitive
+
+// number, string, boolean, null, undefined,symbol, bigInt
+
+
+let num =7;
+
+// console.log(typeof num)
+
+let some = "Ashish";
+// console.log(typeof some)
+
+// Reference or non premitive
+
+//  arrary,objects, function
+
+// data type of all the non-premitive datatype is object, function returns function as data type it called as object function
+
+let arr = [7,5,6,9,8,]
+
+console.log(typeof arr);
+
+let emp = {
+    Name: "ashish",
+    email: "ashu@gmail.com",
+    address: "SQM"
+}
+
+console.log(typeof "Name")
+
+let myfunction = function(){
+    console.log("hollo world")
+}
+
+console.log(typeof myfunction)
