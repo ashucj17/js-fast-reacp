a3ec4dfc90ed28ceaa0a779b2d0bc6f1b44610b2
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-02-18 17:07:21 +0530

    data types and its conversion

---
+++ Data types and conversion/typeconversion.js
@@ -0,0 +1,33 @@
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let newValue= Number(score);
+// console.log(typeof newValue);
+// console.log(newValue);
+
+// "33" -> 33
+// "33abc"-> NaN
+//true -> 1; false -> 0
+
+
+let isTrue = 0;
+
+let isBoolean = Boolean(isTrue);
+
+// console.log(Boolean(isTrue));
+// console.log(typeof isBoolean);
+
+
+let someString = "33";
+
+let newString = String(someString);
+
+// console.log(newString);
+// console.log(typeof newString);
+
+let isLoggedIn = "";
+
+console.log(typeof isLoggedIn);
+console.log(isLoggedIn)
