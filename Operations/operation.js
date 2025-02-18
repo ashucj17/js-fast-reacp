9c5d901c1a4723199fa426e40aaa40fd50a65a4e
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-02-18 20:49:52 +0530

    post and pre increment operator

---
+++ Operations/operation.js
@@ -0,0 +1,26 @@
+// let num = true;
+// console.log(num); // Output: true
+
+
+// let num = +true;
+// console.log(num)  // Output: 1
+
+// let num = +false;
+// console.log(num) // Output: 0
+
+
+let num = false;
+console.log(num)  // Output: false
+
+
+// post and pre increment and
+
+let x=3;
+const y=x++;
+
+console.log(x,y) // Output: x=4,y=3;
+
+let a=2;
+let b =++a;
+console.log(a, b) // Output: a=3,b=3;
+
