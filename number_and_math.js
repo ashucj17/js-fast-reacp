a1058ff5e2dd2a115c46fc2b0be6178d8eab2e1b
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-02-19 17:42:50 +0530

    number and math

---
+++ number_and_math.js
@@ -0,0 +1,38 @@
+let score = 400;
+
+let newScore = new Number(400);
+
+// console.log(score);
+// console.log(newScore)
+
+let balance = 7458;
+// console.log(balance.toString()) 
+// console.log(balance.toString().length)
+
+let floatNumber= 345.678;
+// console.log(floatNumber.toPrecision(4))
+
+let hundredValue= 100000000;
+// console.log(hundredValue.toLocaleString('en-IN'))
+
+
+//+++++++++++++++++++++++++++ Maths++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-7))
+// console.log(Math.round(4.564))
+// console.log(Math.ceil(5.21))
+// console.log(Math.floor(5.68))
+// console.log(Math.PI)
+// console.log(Math.min(5,4,9,7,6,2))
+// console.log(Math.max(5,4,9,7,6,2))
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10 +1 );
+// console.log(Math.floor(Math.random()*10)+1)
+
+let min = 1;
+let max = 6;
+
+console.log(Math.floor(Math.random()* (max-min + 1)+min))
