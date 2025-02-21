015ba0f1be6af375ccf954af27ecb20b6e280f51
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-02-21 18:38:20 +0530

    array basic methods

---
+++ array.js
@@ -0,0 +1,26 @@
+let arr = [0,1,2,3,4,5]
+// console.log(arr)
+// console.log(arr[2])
+// arr.push(7)
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+
+// arr.unshift(8);
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+
+// console.log(arr.includes(5))
+// console.log(arr.indexOf(4))
+
+// let newArr = arr.join()
+// console.log(newArr)
+// console.log(typeof newArr)
+
+//============== slice and splice============
+
+console.log(arr.slice(1,3))
+console.log("A", arr)
+console.log(arr.splice(1,3))
+console.log("B", arr)
