18454df0210849b6d92080e2b7bc99d1e7ac4084
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-02-25 12:38:44 +0530

    singleton object and object literals

---
+++ object2.js
@@ -0,0 +1,11 @@
+let singletonObject = new Object()
+// when we create object using new keyword is calld as singleton object/
+
+let myObj = {}
+//  when we create object by declaring it using literals (let, var, const) is called object literals
+
+myObj.name = "Ashish";
+myObj.age = 30
+myObj.address =  'kanpur';
+
+// console.log(myObj)
