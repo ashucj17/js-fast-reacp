b12b75639d7feb63b4cc606258408fd6096a7071
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-02-26 14:54:45 +0530

    scope of let var and const

---
+++ scopes.js
@@ -0,0 +1,17 @@
+let a =10;
+const b=20;
+// var c = 300;
+// console.log(c)
+if(true)
+    {
+        // var c= 30;
+        let a =10;
+        console.log('Inner:',a);
+
+    }
+
+console.log(a)
+console.log(b)
+// console.log(c)
+
+// var have the global scope and let and const have the block scope
