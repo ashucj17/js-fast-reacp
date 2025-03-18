e9d18810cf930d782afbc75119a980ac7f9ddb41
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-03-18 10:35:37 +0530

    while and do while

---
+++ 2_loops.js
@@ -0,0 +1,24 @@
+let num= 0
+while(num< 10){
+    // console.log(`value is ${num}`)
+    num = num + 2
+}
+
+
+let hero = ["Akshay", "Salman", "Shahrukh", "Sanjay"]
+
+let arr = 0
+while(arr < hero.length){
+    // console.log(`${hero[arr]} is a hero`);
+    arr =arr+1;
+}
+
+
+// exit control loop : do while
+
+let count =11;
+
+do{
+    console.log(`Count is ${count}`)
+    count++;
+}while(count<=10)
